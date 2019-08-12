e249a53b3a4c39f20205fd2e4f97bf47d2ca4676
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-08-12 13:19:00 +0300

    Update Spinner.js

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import classes from './Spinner.css'
-const spinner = () => (
-    <div className={classes.Loader}>Loading...</div>
-);
+
+const spinner = (props) => {
+
+    let spinnerClass =  classes.Loader_Dark ;
+    if(!props.screenMode)
+    spinnerClass =  classes.Loader_Light ;
+
+    return <div className={spinnerClass}>Loading...</div>
+};
 
 
 export default spinner;
