1e765f62c3a8d6967817795a02ac399c1d6d5805
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-08-12 13:23:09 +0300

    Update Resources.js

--- src/Resources/Resources.js
+++ src/Resources/Resources.js
@@ -1,4 +1,4 @@
-export const apiKey = 'dFm0BGa64q0iLgQzEAY7ZIp5VneGFGgz';
+export const apiKey = 'X3HEQ29rsGGGd0virDK8oZOqDRjiAafo';
 export const AutocompleteUrl = 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete';
 export const CurrentConditionsUrl = 'https://dataservice.accuweather.com/currentconditions/v1';
 export const five_Days_Daily_Forecasts_Url = 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/';
